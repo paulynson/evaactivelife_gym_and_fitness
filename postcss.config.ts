5b156263a96c98de9dd94dc8925bc2ec64337c83
paulynson <paulabakada@gmail.com>
2023-11-09 03:04:34 +0100

    updated tailwind and postcss

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,13 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [tailwindcss, autoprefixer]
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,61 @@
 /* eslint-disable no-undef */
+// /** @type {import('tailwindcss').Config} */
+
+// const plugin = require("tailwindcss/plugin");
+
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       textShadow: {
+//         sm: "0 1px 2px var(--tw-shadow-color)",
+//         DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+//         lg: "0 8px 16px var(--tw-shadow-color)",
+//       },
+//     },
+//   },
+//   plugins: [
+//     plugin(function ({ matchUtilities, theme }) {
+//       matchUtilities(
+//         {
+//           "text-shadow": (value) => ({
+//             textShadow: value,
+//           }),
+//         },
+//         { values: theme("textShadow") }
+//       );
+//     }),
+//   ],
+// };
+
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 
-export default {
+const tailwindConfig = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       textShadow: {
         sm: "0 1px 2px var(--tw-shadow-color)",
         DEFAULT: "0 2px 4px var(--tw-shadow-color)",
-        lg: "0 8px 16px var(--tw-shadow-color)",
-      },
-    },
+        lg: "0 8px 16px var(--tw-shadow-color)"
+      }
+    }
   },
   plugins: [
-    plugin(function ({ matchUtilities, theme }) {
+    plugin(({ matchUtilities, theme }) => {
       matchUtilities(
         {
           "text-shadow": (value) => ({
-            textShadow: value,
-          }),
+            textShadow: value
+          })
         },
         { values: theme("textShadow") }
       );
-    }),
-  ],
+    })
+  ]
 };
+
+export default tailwindConfig;
